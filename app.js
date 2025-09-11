b2a0b808709a5337be6cf9141e90ad1a9de68c62
Hugo Luna <hugo94ronal@gmail.com>
2025-09-11 22:17:34 +0000

    chore: initial commit with package.json and basic Node.js server

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.write("On the way to beign a full stack engineer!");
+  res.end();	
+});
+
+server.listen(PORT);
+console.log("Server started on port 3000");
