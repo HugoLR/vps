c51a629a60ed0bd51965452a484316ad2c878518
Hugo Luna <hugo94ronal@gmail.com>
2025-09-12 20:01:28 -0600

    refactor: use express to handle request in the server

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const http = require('http');
+// const http = require('http');
+// const PORT = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.write("On the way to beign a full stack engineer!");
+//   res.end();	
+// });
+
+// server.listen(PORT);
+// console.log("Server started on port 3000");
+
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.write("On the way to beign a full stack engineer!");
-  res.end();	
-});
+const server = require('http').createServer();
 
-server.listen(PORT);
-console.log("Server started on port 3000");
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+})
+
+server.on('request', app);
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
